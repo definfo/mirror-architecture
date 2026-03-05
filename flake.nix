7751a108786c35c3ac0a84e2ca92d48995131750
definfo <comonad@a-stable.com>
2026-03-06 00:16:27 +0800

    feat: init

---
+++ flake.nix
@@ -0,0 +1,82 @@
+{
+  description = "A Nix-flake-based Typst development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    pre-commit-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.treefmt-nix.flakeModule
+        inputs.pre-commit-hooks.flakeModule
+      ];
+
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+
+      perSystem =
+        {
+          config,
+          pkgs,
+          ...
+        }:
+        {
+          # https://flake.parts/options/treefmt-nix.html
+          # Example: https://github.com/nix-community/buildbot-nix/blob/main/nix/treefmt/flake-module.nix
+          treefmt = {
+            projectRootFile = "flake.nix";
+            settings.global.excludes = [ ];
+
+            programs = {
+              autocorrect.enable = true;
+              nixfmt.enable = true;
+              typstyle.enable = true;
+            };
+          };
+
+          # https://flake.parts/options/git-hooks-nix.html
+          # Example: https://github.com/cachix/git-hooks.nix/blob/master/template/flake.nix
+          pre-commit.settings.configPath = ".pre-commit-config.flake.yaml";
+          pre-commit.settings.hooks = {
+            commitizen.enable = true;
+            eclint.enable = true;
+            treefmt.enable = true;
+          };
+
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [
+              config.treefmt.build.devShell
+              config.pre-commit.devShell
+            ];
+
+            shellHook = ''
+              echo 1>&2 "Welcome to the development shell!"
+            '';
+
+            packages = with pkgs; [
+              typst
+              tinymist
+              pandoc
+            ];
+          };
+        };
+    };
+}
